2cc2153c1bb4c3e08cafc1b4b6da7313b79800a5
Yash Paghadar <yashkpaghadar1809@gmail.com>
2026-03-26 15:52:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const admin = require('firebase-admin');
 
-// Firebase setup
-const admin = require("firebase-admin");
-
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
-});
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
